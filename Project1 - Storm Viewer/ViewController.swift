142f30089270a9b8682467a3b650ae48b021e83e
John Kim <cloudiosx@gmail.com>
2022-01-15 08:05:11 -0500

    Initial Commit

---
+++ Project1 - Storm Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1 - Storm Viewer
+//
+//  Created by John Kim on 1/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
